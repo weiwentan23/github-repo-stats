9563fad5b788b516090f211f4b322c2864dc97fd
weiwentan23 <weiwentan@shimmersensing.com>
2022-06-15 22:23:58 +0800

    test

--- analyze.py
+++ analyze.py
@@ -210,7 +210,7 @@ def summarize_data():
 
     # average over past seven days
 
-    data = [df_agg_clones["clones_total"].sum(), df_agg_clones["clones_unique"].sum(), df_agg_views["views_total"].sum(), df_agg_views["views_unique"].sum(), df_stargazers["stars_cumulative"].max(), df_forks["forks_cumulative"].max(), df_agg_clones.["clones_total"].tail(3).mean()]
+    data = [df_agg_clones["clones_total"].sum(), df_agg_clones["clones_unique"].sum(), df_agg_views["views_total"].sum(), df_agg_views["views_unique"].sum(), df_stargazers["stars_cumulative"].max(), df_forks["forks_cumulative"].max(), df_agg_clones["clones_total"].tail(3).mean()]
     columns = ['cumulative_clones_total','cumulative_clones_unique','cumulative_views_total','cumulative_views_unique','cumulative_stars','cumulative_forks', 'test']
     if exists(md_report_filepath):
         df_current = pd.read_csv(md_report_filepath, index_col=0)
