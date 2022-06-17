cca2a9424cd611530e1e3835ccb1c40aa6ff5a18
Tan Wei Wen <weiwentan@shimmersensing.com>
2022-06-17 02:51:18 +0100

    test

--- analyze.py
+++ analyze.py
@@ -246,7 +246,8 @@ def summarize_data():
         MD_SUMMARY.write(
             textwrap.dedent(
                 f"""
-        | {df_agg_clones["index"].iloc[x]} | {round(df_agg_clones["clones_total"].iloc[x:x+7].mean(), 2)}|{round(df_agg_clones["clones_unique"].iloc[x:x+7].mean(), 2)}|{round(df_agg_views["views_total"].iloc[x:x+7].mean(), 2)}|{round(df_agg_views["views_unique"].iloc[x:x+7].mean(), 2)}|
+        | df_agg_clones["time"].iloc(x) | {round(df_agg_clones["clones_total"].iloc[x:x+7].mean(), 2)}|{round(df_agg_clones["clones_unique"].iloc[x:x+7].mean(), 2)}|{round(df_agg_views["views_total"].iloc[x:x+7].mean(), 2)}|{round(df_agg_views["views_unique"].iloc[x:x+7].mean(), 2)}|
+        
         {x}
         {len(df_agg_clones)}
         """
