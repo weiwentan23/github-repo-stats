0b6387278385b25d166c48efddec4bbd4c2e1e75
Tan Wei Wen <weiwentan@shimmersensing.com>
2022-06-17 06:49:07 +0100

    test

--- analyze.py
+++ analyze.py
@@ -251,10 +251,10 @@ def summarize_data():
     days = delta.astype('timedelta64[D]').astype(int)
 
     for i in range(days):
-        day = pd.to_datetime(df_agg_views.index.values[0]) + timedelta(days=i)
+        day = pd.to_datetime(df_agg_views["time"].iloc[0]) + timedelta(days=i)
         if day not in df_agg_views.index:
             log.info(df_agg_views.head())
-            df_agg_views.loc[day] = [day, 0, 0]
+            df_agg_views.loc[-1] = [day, 0, 0]
     log.info(df_agg_views)
     df = df_agg_views.sort_index()
 
