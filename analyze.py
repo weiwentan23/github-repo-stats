3db1b933c7f5a512920a2f9a3c591b7e52f4b697
Tan Wei Wen <weiwentan@shimmersensing.com>
2022-06-18 14:26:55 +0100

    test

--- analyze.py
+++ analyze.py
@@ -242,18 +242,15 @@ def summarize_data():
             df_agg_clones.index = df_agg_clones.index + 1
     df_new_agg_views = df_agg_views.sort_values(by='time',ascending=True)
     df_new_agg_clones = df_agg_clones.sort_values(by='time',ascending=True)
-
-    log.info(df_stargazers)
-    log.info(df_forks)
+    
+    cum_forks = 0
+    cum_stars = 0
     for x in range(0, len(df_new_agg_views), 7):
         for y in range(0, len(df_forks)):
-            cum_forks = 0
             if df_new_agg_views["time"].iloc[x] >= df_forks.index.date[y]:
                 cum_forks = df_forks["forks_cumulative"].iloc[y]
         for z in range(0, len(df_stargazers)):
-            cum_stars = 0
             if df_new_agg_views["time"].iloc[x] >= df_stargazers.index.date[z]:
-                log.info("fdsafdsafsda")
                 cum_stars = df_stargazers["stars_cumulative"].iloc[z]
         MD_SUMMARY.write(
             textwrap.dedent(
